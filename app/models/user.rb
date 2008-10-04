fcbd3cbdb9d86ad199d5a1bdf70542b945316356
unknown <yjcqwliu@yahoo.com.cn>
2008-10-04 21:43:56 +0800

    feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,7 @@ class User < ActiveRecord::Base
 	serialize :invite
 	
     has_many :usership
+	has_many :notice
 	
 	
 	
--- script/feed_runner.rb
+++ script/feed_runner.rb
@@ -1,27 +1,26 @@
 current_time = Time.now
-
-User.find(:all, :conditions => ["  sent = false " ]).each do |user|
+puts "start"
+puts Notice.find(:all, :conditions => ["  sented is null or noticed is null " ],:order => " updated_at desc ").length
+Notice.find(:all, :conditions => [" sented is null or noticed is null " ],:order => " updated_at desc ").each do |notice|
   begin
-    #notifications = [
-    #                 "<xn:name uid='#{user.user.xid}'/>在美容达人PK赛向你发出了站题，<a href='http://apps.xiaonei.com/beautypk/challenge/myreceive'>回答这些问题就可以和TA一决胜负了</a>"
-     #               ]
-    #res_note = user.user.xn_session.invoke_method("xiaonei.notifications.send", 
-       #                                             :to_ids => [user.target_xid].join(","), 
-       #                                             :notification => notifications.rand)
+   # notifications = ["<xn:name uid='#{friend.user.xid}'/>在美容达人PK赛向你发出了站题，<a href='http://apps.xiaonei.com/beautypk/challenge/myreceive'>回答这些问题就可以和TA一决胜负了</]
+    res_note = notice.user.xn_session.invoke_method("xiaonei.notifications.send", 
+                                                    :to_ids => notice.to_xid, 
+                                                    :notification => notice.content)
     
-    res_feed = user.xn_session.invoke_method("xiaonei.feed.publishTemplatizedAction", 
+    res_feed = notice.user.xn_session.invoke_method("xiaonei.feed.publishTemplatizedAction", 
                                                     :title_data => { 
                                                     }.to_json,
                                                     :body_data => { 
-                                                      :my_uid => user.xid,
-                                                      :age => user.age,
+                                                      :content => notice.content
                                                     }.to_json,
                                                     :template_id => 1)
     
-    puts "#{current_time}: process user #{user.id}:  #{res_feed.inspect}"
+    puts "#{current_time}: process user #{notice.user.id}:  #{res_feed.inspect} #{res_note.inspect}"
     
-    user.sent = true
-    user.save
+    notice.sented = true
+	notice.noticed = true
+    notice.save
   rescue Exception => exp
     puts "exp: #{exp.inspect}"
   end
