5e3c90cb4da6f85b5a5fa261b2e7358fbdf95166
unknown <Administrator@.(none)>
2008-11-06 11:07:34 +0800

    update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -28,10 +28,10 @@ module ApplicationHelper
 	def show_notice(notice)
        case notice.ltype
 	   when 1
-	       re = ["#{url_to_island(notice.from_xid)}船长驾驶他的#{notice.column2}在#{url_to_island(notice.to_xid)}的岛上烧杀抢掠一番，抢劫了#{notice.column1}金币","#{url_to_island(notice.from_xid)}船长一边唱着“我是个大盗贼，什么都不怕”一边开着他的#{notice.column2}带着从#{url_to_island(notice.to_xid)}的岛上抢来的#{notice.column1}金币和一群美女悠闲的离开了"].rand
+	       re = ["#{url_to_island(notice.from_xid)}船长驾驶他的#{h notice.column2}在#{url_to_island(notice.to_xid)}的岛上烧杀抢掠一番，抢劫了#{h notice.column1}金币","#{url_to_island(notice.from_xid)}船长一边唱着“我是个大盗贼，什么都不怕”一边开着他的#{h notice.column2}带着从#{url_to_island(notice.to_xid)}的岛上抢来的#{h notice.column1}金币和一群美女悠闲的离开了"].rand
 	   
 	    when 2
-	       re = ["#{url_to_island(notice.from_xid)}船长成功的击退了#{url_to_island(notice.to_xid)}的进攻，夺回了#{notice.column1}金币","#{url_to_island(notice.to_xid)}被#{url_to_island(notice.from_xid)}船长一炮打回老家，抢劫行动宣告失败，留下了#{notice.column1}金币归#{url_to_island(notice.from_xid)}所有了"].rand
+	       re = ["#{url_to_island(notice.from_xid)}船长成功的击退了#{url_to_island(notice.to_xid)}的进攻，夺回了#{h notice.column1}金币","#{url_to_island(notice.to_xid)}被#{url_to_island(notice.from_xid)}船长一炮打回老家，抢劫行动宣告失败，留下了#{h notice.column1}金币归#{url_to_island(notice.from_xid)}所有了"].rand
 	   
 	   when 3
 	       re = "#{url_to_island(notice.from_xid)}接受了邀请，加入了海盗时代，#{url_to_island(notice.to_xid)}获得3000金币和1海盗币"
